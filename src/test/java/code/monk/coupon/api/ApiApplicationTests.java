9ed701b5ad94b4ae2e29ef185556b5835a214614
reharsh67 <reharshdeshpande@gmail.com>
2025-01-23 14:58:00 +0530

    initial commit

---
+++ src/test/java/code/monk/coupon/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package code.monk.coupon.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
